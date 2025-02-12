c5e1d90639aaf386ff137dbc7836c403060c3414
ubong-web <ubongj368@gmail.com>
2025-02-12 20:05:23 +0100

    updated compose and routes

--- api/routes/books.py
+++ api/routes/books.py
@@ -40,6 +40,9 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
+@router.get("/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK)
+async def get_books() -> OrderedDict[int, Book]:
+    return db.get_books()
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int) -> Book:
